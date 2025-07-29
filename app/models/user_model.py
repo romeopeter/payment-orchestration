ba13b0f6f1083e3132e538ad8dd0f97c0da750c5
romeo <romeomeshack34@gmail.com>
2025-07-29 22:39:23 +0100

    DB models

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -4,6 +4,8 @@ from datetime import datetime
 # ----------------------------------
 
 class User(db.Model):
+    """User DB model"""
+    
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(20), unique=True, nullable=False)
     password_hash = db.Column(db.String(120), nullable=False)
