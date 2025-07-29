7af3c1c14eddd6c46c6514cf51c6a184f68770d8
romeo <romeomeshack34@gmail.com>
2025-07-29 21:42:23 +0100

    Model class

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from app.extensions import db
+from datetime import datetime
+
+# ----------------------------------
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(20), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
