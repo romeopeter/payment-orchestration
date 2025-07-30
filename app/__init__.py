9c99aa9d1e0be38f3706b6e5a0d577f512487b0c
romeo <romeomeshack34@gmail.com>
2025-07-30 11:10:51 +0100

    app init

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from .extensions import db, jwt
+from .routes.auth import auth_bp
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("app.config.Config")
+
+    db.init_app(app)
+    jwt.init_app(app)
+
+    app.register_blueprint(auth_bp, url_prefix="/api/auth")
+
+    return app
