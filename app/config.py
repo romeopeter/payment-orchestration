52a4740a5927e64ff9afc699f51b65a6413dfe16
romeo <romeomeshack34@gmail.com>
2025-08-03 02:18:10 +0100

    Added condfig for Swagger

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class Config:
     """App server configuration"""
 
     SECRET_KEY = os.getenv("SECRET_KEY", "super-classified-key")
-    SQLALCHEMY_DATABASE_URL = "sqlite:///db.sqlite3"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt-secret")
     SWAGGER = {"title": "Payment Orchestration API", "uiversion": 3, "openapi": "3.0.2"}
