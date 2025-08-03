3df7a38569e12401b1a3eaa5f40cf4285546bb98
romeo <romeomeshack34@gmail.com>
2025-08-03 22:39:16 +0100

    corrected field attributes

--- app/models/transaction_model.py
+++ app/models/transaction_model.py
@@ -1,20 +1,19 @@
 from app.extensions import db
 from sqlalchemy.dialects.sqlite import JSON
+from sqlalchemy import Column, Integer, DateTime, String
 from datetime import datetime
 
 # -------------------------------------------
 
 
 class Transaction(db.Model):
-    db = db.Column(db.Integer, primary_key=True)
-    reference = db.Column(
-        db.String(64), unique=True, nullable=False
-    )  # Unique gateway ref
-    amount = db.Column(db.Integer, nullable=False)
-    gateway = db.Column(db.String(32), nullable=False)
-    status = db.Column(
-        db.String(10), nullable=False, unique=False, default="pending"
+    id = Column(Integer, primary_key=True)
+    reference = Column(String(64), unique=True, nullable=False)  # Unique gateway ref
+    amount = Column(Integer, nullable=False)
+    gateway_ref = Column(String(32), nullable=False)
+    status = Column(
+        String(10), nullable=False, unique=False, default="pending"
     )  # pending, failed, "success"
-    metadata = db.Column(db.JSON, nullable=True)
-    customer_id = db.column(db.Integer, db.Foreign_key("user.id"), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.now)
+    txn_metadata = Column(JSON, nullable=True)  # Unstructured JSON data
+    customer_id = Column(Integer, db.ForeignKey("user.id"), nullable=False)
+    created_at = Column(DateTime, default=datetime.now)
